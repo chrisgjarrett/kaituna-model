278a7f4e7096d0791207b630edf4b1a3bc641d02
Chris Jarrett <chris.g.jarrett@gmail.com>
2022-11-12 11:43:45 +0000

    #Dockerfile updates

--- web_app/app.py
+++ web_app/app.py
@@ -1,4 +1,3 @@
-import importlib
 from datetime import datetime, timedelta, date
 
 import pandas as pd
@@ -7,17 +6,13 @@ from web_scraper import kaituna_web_scraper
 from data_preprocessing import data_aggregation
 import pickle as pk
 import tensorflow as tf
-import sys
-import os
 
 # web frameworks
 from flask import Flask
-from starlette.applications import Starlette
-from starlette.responses import JSONResponse, HTMLResponse, RedirectResponse
-import uvicorn
 
-#NUMBER OF DAYS TO LOOK BACK
+# NUMBER OF DAYS TO LOOK BACK
 NUMBER_OF_DAYS_LAG = 0
+
 
 # Preprocess data
 def preprocess_data(X_raw):
@@ -26,32 +21,33 @@ def preprocess_data(X_raw):
     f.close()
     return preprocessor.transform(X_raw)
 
+
 # Takes a start date and end date and returns the features
 def get_features():
     # Predictions require today's data only, currently
     start_date = date.today()
-    end_date = start_date - timedelta(days = NUMBER_OF_DAYS_LAG)
+    end_date = start_date - timedelta(days=NUMBER_OF_DAYS_LAG)
 
     hourly_df = kaituna_web_scraper.collate_kaituna_data(start_date=start_date, end_date=end_date)
 
     # Aggregate to daily metrics
     X_daily = data_aggregation.aggregate_to_daily(hourly_df)
-    X_daily["IsWeekend"] = X_daily.apply(data_aggregation.is_weekend, axis=1) # todo: this is probably wrong
+    X_daily["IsWeekend"] = X_daily.apply(data_aggregation.is_weekend, axis=1)  # todo: this is probably wrong
 
     # Get relevant features
     X_daily = X_daily[["FlowRate", "Rainfall", "LakeLevel", "IsWeekend"]]
 
     # Rename columns
-    X_daily.columns=["Target_lag","Rainfall_lag","LakeLevel_lag", "IsWeekend"]
+    X_daily.columns = ["Target_lag", "Rainfall_lag", "LakeLevel_lag", "IsWeekend"]
 
     return X_daily
 
-#app = Starlette()
+
 app = Flask(__name__)
+
 
 @app.route("/predict-flow", methods=["GET"])
 def predict_flow():
-
     # Get raw features
     X = get_features()
 
@@ -71,9 +67,9 @@ def predict_flow():
             </body>
         </figure>
         </html>
-        """ % (date.today(), y_preds[0,0],
-               date.today() + timedelta(days=1), y_preds[0,1],
-               date.today() + timedelta(days=2), y_preds[0,2])
+        """ % (date.today(), y_preds[0, 0],
+               date.today() + timedelta(days=1), y_preds[0, 1],
+               date.today() + timedelta(days=2), y_preds[0, 2])
 
 
 @app.route("/")
@@ -94,6 +90,6 @@ def redirect_to_homepage(request):
 """
 
 if __name__ == "__main__":
-    #port = int(os.environ.get("PORT", 8008))
-    #uvicorn.run(app)
-    app.run(host='0.0.0.0')
+    # port = int(os.environ.get("PORT", 8008))
+    # uvicorn.run(app)
+    app.run()

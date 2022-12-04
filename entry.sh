57d1c8b6e69c4026db531e98f01f2d5cb056eede
Chris Jarrett <chris.jarrett@acurable.com>
2022-12-04 22:50:51 +0000

    Deleting entry script

--- entry.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if [ -z "${AWS_LAMBDA_RUNTIME_API}" ]; then
-    exec /usr/bin/aws-lambda-rie /usr/local/bin/python -m handle_predictions $1
-else
-    exec /usr/local/bin/python -m handle_predictions $1
-fi
